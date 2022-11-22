8f01b7397213fe88370853aa98c6de025dc3801e
Jon Gunderson <jongund@illinois.edu>
2022-11-22 13:22:26 -0600

    added skipto feature to website

---
+++ skipto-config.js
@@ -0,0 +1,14 @@
+/* skipto-config.js */
+
+var SkipToConfig =  {
+    headings: 'main h1 h2',
+    positionLeft: '44rem',
+    fontSize: '14px',
+    menuTextColor: '#00132c',
+    menuBackgroundColor: '#cad9ef',
+    menuitemFocusTextColor: '#eeeeee',
+    menuitemFocusBackgroundColor: '#00132c',
+    focusBorderColor: '#ff552e',
+    buttonTextColor: '#444444',
+    buttonBackgroundColor: '#dddede',
+};
